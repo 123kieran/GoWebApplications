a3b2926644f09cb983682af04c80888013e3452f
123kieran <123kieran@gmail.com>
2017-10-26 16:42:29 +0100

    add template and cookie

--- GuessingGame.go
+++ GuessingGame.go
@@ -1,21 +1,62 @@
-// Problem 2
+
 // Kieran O'Haloran 25/10/17
 
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"math/rand"
+	"time"
+	"strconv"
 )
 
+type myMsg struct{
+Message string
+}
+
+func server(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type","text/html")
+
+	http.ServeFile(w, r, "index.html")
+}
+
+func guessHandler(w http.ResponseWriter, r *http.Request){
+
+	//http.ServeFile(w, r, "guess.html")
+
+		message :="Guess a number between 1 and 20"
+		
+			rand.Seed(time.Now().UTC().UnixNano())
+		
+			target:=0
+			var cookie, err = r.Cookie("target")
+		
+			if err == nil{
+				
+				target, _ = strconv.Atoi(cookie.Value)
+				if target ==0{
+					target = rand.Intn(20-1)
+				}
+			}
+		
+			cookie = &http.Cookie{
+				Name: "target",
+				Value: strconv.Itoa(target),
+				Expires: time.Now().Add(72 * time.Hour),
+			}
+			
+			http.SetCookie(w,cookie)
+			
+			t, _ := template.ParseFiles("guess.tmpl")
+
+			t.Execute(w, &myMsg{Message:message})
+}
 
 
-func server(w http.ResponseWriter, r *http.Request){
-	//fmt.Fprintf(w, "<h1>Guessing Game</h1>")
-	http.ServeFile(w, r, "guess.html")}
-
-
-func main() {
-	http.HandleFunc("/", requestHandler)
-	http.ListenAndServe(":8080", nil)
+func main(){
+	http.HandleFunc("/", server)//"/"handles any requests and passes to server
+	http.HandleFunc("/guess", guessHandler)
+	http.ListenAndServe(":8080", nil)//webserver is started
 }
